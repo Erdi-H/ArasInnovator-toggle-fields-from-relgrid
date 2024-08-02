27d2cfd81586d3f3befa309f21c5c6473f9794ea
Erdi-H <97852615+Erdi-H@users.noreply.github.com>
2024-08-02 08:37:11 -0400

    Create visibility-onInsertRow.cs

---
+++ visibility-onInsertRow.cs
@@ -0,0 +1,39 @@
+/******************************************************************************
+Revisions:
+    Rev Date        Modified By      Description
+*******************************************************************************/
+var parentItem = parent.thisItem;
+var inn = parentItem.getInnovator();
+
+var parentForm = parent.parent.document.getElementById("instance");
+var parentFormWindow = parentForm.contentWindow;
+
+if (parentFormWindow) { // only run when this var is defined
+    // get the fields you want to toggle
+    var modelYearField = parentFormWindow.getFieldByName("model_year");
+    var programPhaseField = parentFormWindow.getFieldByName("program_phase");
+    
+    var relatedItems = parentItem.getItemsByXPath("//Item[@type='issue_program']");
+    var relatedCount = relatedItems.getItemCount();
+    var classifications = [];
+    for (let i = 0; i < relatedCount; ++i) {
+        var currItem = relatedItems.getItemByIndex(i);
+        var classification = currItem.getAttribute("classification");
+        if (!classification) {
+            var itemHTML = currItem.node.innerHTML;
+            var classificationMatch = itemHTML.match(/<classification>(.*?)<\/classification>/);
+            classification = classificationMatch ? classificationMatch[1] : null;
+        }
+        classifications.push(classification);
+    }
+    var hasWiring = classifications.includes("Wiring");
+    
+    if (hasWiring) {
+        modelYearField.style.visibility = 'visible';
+        programPhaseField.style.visibility = 'visible';  
+    }
+    else {
+        modelYearField.style.visibility = 'hidden';
+        programPhaseField.style.visibility = 'hidden';
+    }
+}
